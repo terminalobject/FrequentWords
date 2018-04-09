9799246ca6955cd06737aec39bca183b2f9f3621
Alessandro Noiato <doctorboredom@hotmail.com>
2018-04-09 18:42:44 +0200

    cut some code

--- src/main/java/com/m3c/an/Stream.java
+++ src/main/java/com/m3c/an/Stream.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.KeyStore;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Stream {
 
@@ -23,7 +25,7 @@ public class Stream {
         }
     }
 
-    Consumer<String> countWords = word -> map.computeIfAbsent(word, (w) -> new AtomicInteger(0)).incrementAndGet();
+    private Consumer<String> countWords = word -> map.computeIfAbsent(word, (w) -> new AtomicInteger(0)).incrementAndGet();
 
     public void getLines() {
 
@@ -38,75 +40,13 @@ public class Stream {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Sorting");
-        Object[] a = map.entrySet().toArray();
-        Arrays.sort(a, Comparator.comparingInt((Object entry) -> entry.getValue.get()));//(Comparator) (o1, o2) -> ((Map.Entry<String, AtomicInteger>) o2).getValue().get() - (((Map.Entry<String, AtomicInteger>) o2).getValue().get()));
-
-
-//        Arrays.sort(a, new Comparator() {
-//            public int compare(Object o1, Object o2) {
-//                return ((Map.Entry<String, AtomicInteger>) o2).getValue()
-//                        .compareTo(((Map.Entry<String, AtomicInteger>) o1).getValue());
-//            }
-//        })
-//
-        for (int i = 0; i < 3; i++) {
-            System.out.println(((Map.Entry<String, AtomicInteger>) a[i]).getKey() + " : "
-                    + ((Map.Entry<String, AtomicInteger>) a[i]).getValue());
+        List<Map.Entry<String, AtomicInteger>> orderedList = sortedEntries(map);
+        for (int i = orderedList.size() - 1; i > orderedList.size() - 4; i--) {
+            System.out.println(orderedList.get(i).getKey() + " : "
+                    + orderedList.get(i).getValue());
         }
     }
 
-
-        //List<Map.Entry<String, String>> result = map.
-//        int highest = highestFrequency(map);
-//        String[] mostCommonWords = new String[highest];
-//        for (String key : map.keySet()) {
-//            if (map.get(key) > 10) {
-//                mostCommonWords[map.get(key) - 1] = "";
-//                mostCommonWords[map.get(key) - 1] += key;
-//            }
-//        }
-//
-//
-//        int printCounter = 0;
-//        for (int i=highest - 1; i > 10; i--) {
-//            if (mostCommonWords[i] != null && printCounter < 3) {
-//                    printCounter++;
-//                    System.out.println(mostCommonWords[i] + " Count " + (i)); }
-//        }
-
-//        Map<String, Integer> sortedWords = new TreeMap<>(map);
-//        for (Object key : sortedWords.keySet()) {
-//            System.out.println("Word: " + key + "\tCounts: " + map.get(key));
-//        }
-
-
-    public Map<String, Integer> sortByValue(Map<String, Integer> map) {
-            List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
-
-            Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-
-                public int compare(Map.Entry<String, Integer> m1, Map.Entry<String, Integer> m2) {
-                    return (m2.getValue()).compareTo(m1.getValue());
-                }
-            });
-
-            Map<String, Integer> result = new LinkedHashMap<String, Integer>();
-            for (Map.Entry<String, Integer> entry : list) {
-                result.put(entry.getKey(), entry.getValue());
-            }
-            return result;
-        }
-
-    public int highestFrequency(Map<String, Integer> map) {
-        int highestFrequency = 0;
-        for (String key: map.keySet()) {
-            if (map.get(key) > highestFrequency) {
-                highestFrequency = map.get(key);
-            }
-        }
-        return highestFrequency;
-    }
     public void lineToWordsToMap(String line, Consumer<String> countWords) {
         char[] characters = line.toCharArray();
         StringBuilder sb = new StringBuilder(16);
@@ -133,6 +73,14 @@ public class Stream {
     }
     public Map<String, AtomicInteger> getMap() {
         return this.map;
+    }
+
+    private static List<Map.Entry<String, AtomicInteger>> sortedEntries(
+            Map<String, AtomicInteger> map) {
+        return map.entrySet()
+                .stream()
+                .sorted(Comparator.comparingInt(entry -> entry.getValue().get()))
+                .collect(Collectors.toList());
     }
 }
 
